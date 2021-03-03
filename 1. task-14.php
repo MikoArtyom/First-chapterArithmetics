bd1775cb2365e6d8e16f499bf99e50fddece641d
Artem99 <pasha.pikav@mail.ru>
2021-03-03 15:30:48 +0300

    completed 14 task

--- 1. task-14.php
+++ 1. task-14.php
@@ -1,1 +1,29 @@
 <?php
+$countIteration = 0;
+for ($i = 1; $i<=250; $i++){
+    for ($j = 1; $j<=250; $j++){
+            if (isSimpleNum($i) && isSimpleNum($j) && ($j - $i == 2)) {
+                $countIteration++;
+
+                if ($countIteration > 10){
+                    exit();
+                }
+                else
+                echo $i . " " . $j . "<br>";
+
+            }
+
+    }
+}
+
+function isSimpleNum($value){
+    $count = 0;
+    for ($i = 1; $i<=$value; $i++){
+        if ($value%$i == 0){
+            $count++;
+        }
+    }
+    if ($count==2){
+        return true;
+    }
+}
