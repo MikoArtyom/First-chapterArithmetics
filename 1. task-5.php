29be1ff0f4fa90448c8bd8dac195d9adae698a75
Artem99 <pasha.pikav@mail.ru>
2021-03-05 14:09:14 +0300

    update 5 task

--- 1. task-5.php
+++ 1. task-5.php
@@ -1,9 +1,9 @@
 <?php
 $n = intval(1234);
-$k = "";
+$k = 0;
 
 while ($n != 0){
-    $k .= strval($n%10);
-    $n = intval($n/10);
+    $k =$k*10 + strval($n % 10);
+    $n = intval($n / 10);
 }
 echo $k;
