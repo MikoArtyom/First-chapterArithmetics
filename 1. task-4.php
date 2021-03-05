3a25c3a4ee791e0b51357fa5ba77e31c781c3fd3
Artem99 <pasha.pikav@mail.ru>
2021-03-05 14:06:47 +0300

    update 4 task

--- 1. task-4.php
+++ 1. task-4.php
@@ -1,13 +1,34 @@
 <?php
-for ($i = 1000; $i<=9999; $i++){
-    if ($i % 2 == 0){
-       $first = intval($i)/1000%10 ;
-       $second = intval($i)/100%10;
-       $third = intval($i)/10%10;
-       $fourth = intval($i%10);
-       if ((($first<$second) && ($second<$third) && ($third<$fourth))
-       || (($first>$second) && ($second>$third) && $third>$fourth)){
-           echo $i . "<br>";
-       }
+$isOrder = true;
+$isDesc = true;
+$countDesc = 0;
+$countOrder = 0;
+for ($i = 1000; $i <= 9999; $i += 2) {
+    $value = $i;
+    $count = 0;
+    while ($value){
+        $count++;
+        $value = intval($value / 10);
     }
+    $value = $i;
+    $first = $value % 10;
+    $value = intval($value / 10);
+    $countDesc = 0;
+    $countOrder = 0;
+    while ($value) {
+        if ($first < ($value % 10)) {
+           $countDesc++;
+        }
+
+        if ($first > ($value % 10)){
+            $countOrder++;
+        }
+
+        $first = intval($value % 10);
+        $value = intval($value / 10);
+    }
+    if ($countOrder == $count-1 || $countDesc == $count-1){
+        echo $i. "<br>";
+    }
+
 }
