032d76f04cc563bb0fb1a42ea8c2ad60e5450c7d
Artem99 <pasha.pikav@mail.ru>
2021-03-09 12:48:34 +0300

    completed 21 task

--- 1. task-21.php
+++ 1. task-21.php
@@ -1,1 +1,25 @@
 <?php
+$n = 33;
+$m = 34;
+
+if (NOD($n,$m) == 1){
+    echo "числа взаимо простые!";
+}
+else{
+    echo "unlucky";
+}
+
+
+function NOD($val1, $val2){
+    while ($val1 != $val2){
+        if ($val1 > $val2){
+            $val1 = $val1 - $val2;
+        }
+        else
+        {
+            $val2 = $val2 - $val1;
+        }
+    }
+    return $val1;
+}
+
