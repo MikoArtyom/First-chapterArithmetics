c6f311a263995ac1cd55963d5868f7f66d504eb2
Artem99 <pasha.pikav@mail.ru>
2021-03-09 12:33:09 +0300

    completed 18 task

--- 1. task-18.php
+++ 1. task-18.php
@@ -1,1 +1,28 @@
 <?php
+$n = 30;
+$m = 500;
+
+for ($i = $n; $i <= $m; $i++) {
+    for ($j = $i + 1; $j <= $m; $j++) {
+
+        $sumDiviserFirst = 0;
+        $sumDiviserSecond = 0;
+
+        for ($k = 1; $k <= $i - 1; $k++) {
+            if ($i % $k == 0) {
+                $sumDiviserFirst += $k;
+            }
+        }
+
+        for ($k = 1; $k <= $j - 1; $k++) {
+            if ($j % $k == 0) {
+                $sumDiviserSecond += $k;
+            }
+        }
+
+        if ($sumDiviserFirst == $j && $i == $sumDiviserSecond) {
+            echo $i . " - " . $j . "<br>";
+        }
+    }
+}
+
